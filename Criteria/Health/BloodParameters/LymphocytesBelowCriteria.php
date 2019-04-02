bf773c381e72a834181eb24deaa495bda46e2c63
Marcin Szołtysek <marcin.szoltysek@thesoftwarehouse.pl>
2019-04-02 09:14:45 +0200

    Missing criterias

---
+++ Criteria/Health/BloodParameters/LymphocytesBelowCriteria.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Criteria\Health\BloodParameters;
+
+use Criteria\PatientCriteriaInterface;
+use Model\Patient\BloodParametersReference;
+use Model\Patient\Patient;
+
+class LymphocytesBelowCriteria implements PatientCriteriaInterface
+{
+    /**
+     * @param Patient $patient
+     *
+     * @return bool
+     */
+    public function match(Patient $patient): bool
+    {
+        return $patient->getBloodParameters()->getLymphocytes()
+            < BloodParametersReference::getLymphocytesMin();
+    }
+}
