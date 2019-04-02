885ac52c25be047ec2ed62c787fa68477f76f204
Marcin Szołtysek <marcin.szoltysek@thesoftwarehouse.pl>
2019-04-02 07:51:56 +0200

    App & print

--- App/app.php
+++ App/app.php
@@ -1,7 +1,24 @@
 <?php
 
+use HealthTest\CardiovascularDiseaseTest;
+use HealthTest\PolycythemiaTest;
+use HealthTest\WellTest;
+use Model\Patient\Patient;
+
 require_once 'autoload.php';
 require_once 'config.php';
+require_once 'print.php';
 require_once 'data-set-generator.php';
 
 $dataSet = prepareDataSet(POPULATION);
+
+$test = new CardiovascularDiseaseTest();
+$test = new PolycythemiaTest();
+$test = new WellTest();
+$sick = $test->testPopulation($dataSet);
+
+echo get_class($test)." matches: ".count($sick)."\n======================\n";
+
+array_map(function(Patient $patient) {
+    echo printPatient($patient)."\n\n";
+}, $sick);
---
+++ App/print.php
@@ -0,0 +1,91 @@
+<?php
+
+use Model\Patient\BloodParameters;
+use Model\Patient\BloodParametersReference;
+use Model\Patient\Patient;
+
+function printPatient(Patient $patient)
+{
+    return sprintf("
+%s %s, age: %d years, %s
+weight: %d kg, height: %d cm, waist: %d cm, chest: %d cm
+systolic pressure: %d, diastolic: %d
+BLOOD PARAMETERS: %s",
+        $patient->getFirstName(),
+        $patient->getLastName(),
+        $patient->getBirthDate()->diff(new \DateTime())->y,
+        $patient->getSex(),
+        $patient->getWeight(),
+        $patient->getHeight(),
+        $patient->getWaistWidth(),
+        $patient->getChestWidth(),
+        $patient->getSystolicPressure(),
+        $patient->getDiastolicPressure(),
+        printBloodParameter($patient->getBloodParameters(), $patient->getSex())
+    );
+}
+
+function printBloodParameter(BloodParameters $parameters, $sex)
+{
+    $data = [
+        [
+            'Erythrocytes',
+            $parameters->getErythrocytes(),
+            BloodParametersReference::getErythrocytesMin($sex),
+            BloodParametersReference::getErythrocytesMax($sex)
+        ], [
+            'Thrombocytes',
+            $parameters->getThrombocytes(),
+            BloodParametersReference::getThrombocytesMin(),
+            BloodParametersReference::getThrombocytesMax(),
+        ], [
+            'Monocytes',
+            $parameters->getMonocytes(),
+            BloodParametersReference::getMonocytesMin(),
+            BloodParametersReference::getMonocytesMax(),
+        ], [
+            'Lymphocytes',
+            $parameters->getLymphocytes(),
+            BloodParametersReference::getLymphocytesMin(),
+            BloodParametersReference::getLymphocytesMax(),
+        ], [
+            'Leukocytes',
+            $parameters->getLeukocytes(),
+            BloodParametersReference::getLeukocytesMin(),
+            BloodParametersReference::getLeukocytesMax(),
+        ], [
+            'Hemoglobin',
+            $parameters->getHemoglobin(),
+            BloodParametersReference::getHemoglobinMin($sex),
+            BloodParametersReference::getHemoglobinMax($sex),
+        ], [
+            'Hematocrit',
+            $parameters->getHematocrit(),
+            BloodParametersReference::getHemoglobinMin($sex),
+            BloodParametersReference::getHemoglobinMax($sex),
+        ], [
+            'MCV',
+            $parameters->getMCV(),
+            BloodParametersReference::getMCVMin($sex),
+            BloodParametersReference::getMCVMax($sex),
+        ], [
+            'MCH',
+            $parameters->getMCH(),
+            BloodParametersReference::getMCHMin(),
+            BloodParametersReference::getMCHMax(),
+        ], [
+            'MCHC',
+            $parameters->getMCHC(),
+            BloodParametersReference::getMCHCMin(),
+            BloodParametersReference::getMCHCMax()
+        ]
+
+    ];
+
+    $desc = '';
+    array_map(function($item) use (&$desc) {
+        $desc .= vsprintf("\n  > %15.15s: %7.2f (%7.2f - %7.2f)", $item);
+    }, $data);
+
+    return $desc;
+}
