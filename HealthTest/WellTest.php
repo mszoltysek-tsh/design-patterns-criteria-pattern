14d68e30329f187bb905af8542ca68d7f69a6444
Marcin Szołtysek <marcin.szoltysek@thesoftwarehouse.pl>
2019-04-02 09:15:40 +0200

    WellTest fix

--- HealthTest/WellTest.php
+++ HealthTest/WellTest.php
@@ -3,15 +3,25 @@
 namespace HealthTest;
 
 use Criteria\Complex\NeitherCriteria;
+use Criteria\Health\BloodParameters\ErythrocytesBelowCriteria;
 use Criteria\Health\BloodParameters\ErythrocytesOverCriteria;
+use Criteria\Health\BloodParameters\HematocritBelowCriteria;
 use Criteria\Health\BloodParameters\HematocritOverCriteria;
+use Criteria\Health\BloodParameters\HemoglobinBelowCriteria;
 use Criteria\Health\BloodParameters\HemoglobinOverCriteria;
+use Criteria\Health\BloodParameters\LeukocytesBelowCriteria;
 use Criteria\Health\BloodParameters\LeukocytesOverCriteria;
+use Criteria\Health\BloodParameters\LymphocytesBelowCriteria;
 use Criteria\Health\BloodParameters\LymphocytesOverCriteria;
+use Criteria\Health\BloodParameters\MCHBelowCriteria;
+use Criteria\Health\BloodParameters\MCHCBelowCriteria;
 use Criteria\Health\BloodParameters\MCHCOverCriteria;
 use Criteria\Health\BloodParameters\MCHOverCriteria;
+use Criteria\Health\BloodParameters\MCVBelowCriteria;
 use Criteria\Health\BloodParameters\MCVOverCriteria;
+use Criteria\Health\BloodParameters\MonocytesBelowCriteria;
 use Criteria\Health\BloodParameters\MonocytesOverCriteria;
+use Criteria\Health\BloodParameters\ThrombocytesBelowCriteria;
 use Criteria\Health\BloodParameters\ThrombocytesOverCriteria;
 
 class WellTest extends AbstractHealthTest
@@ -31,7 +41,18 @@ class WellTest extends AbstractHealthTest
             new MCHCOverCriteria(),
             new MCVOverCriteria(),
             new MonocytesOverCriteria(),
-            new ThrombocytesOverCriteria()
+            new ThrombocytesOverCriteria(),
+
+            new ErythrocytesBelowCriteria(),
+            new HematocritBelowCriteria(),
+            new HemoglobinBelowCriteria(),
+            new LeukocytesBelowCriteria(),
+            new LymphocytesBelowCriteria(),
+            new MCHBelowCriteria(),
+            new MCHCBelowCriteria(),
+            new MCVBelowCriteria(),
+            new MonocytesBelowCriteria(),
+            new ThrombocytesBelowCriteria()
         ]);
     }
 }
